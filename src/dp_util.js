ca654c14c3b4deb8062e33b915a36d757ff4be96
phatsk <phatsk@gmail.com>
2015-03-18 11:31:58 -0400

    More util fixes

--- src/dp_util.js
+++ src/dp_util.js
@@ -1,33 +1,36 @@
-var dp_util = {
-	log: function(prefix, message)
+var dp_util = (function(){
+	function log(prefix, message)
 	{
 		return console.log('[' + prefix + '] ' + message);
-	},
-	logUI: function(message)
-	{
-		return log('UI', message);
-	},
-	logError: function(message)
-	{
-		return log('EE', message);
-	},
-	logInfo: function(message)
-	{
-		return log('II', message);
-	},
-	logRemote: function(message)
-	{
-		return log('<<', message);
-	},
-	logJSON: function(message)
-	{
-		return log('{}', JSON.stringify(message));
-	},
-	logLocal: function(message)
-	{
-		return ENABLE_LOGGING && log('>>', message);
 	}
-};
+
+	return {
+		logUI: function(message)
+		{
+			return log('UI', message);
+		},
+		logError: function(message)
+		{
+			return log('EE', message);
+		},
+		logInfo: function(message)
+		{
+			return log('II', message);
+		},
+		logRemote: function(message)
+		{
+			return log('<<', message);
+		},
+		logJSON: function(message)
+		{
+			return log('{}', JSON.stringify(message));
+		},
+		logLocal: function(message)
+		{
+			return ENABLE_LOGGING && log('>>', message);
+		}
+	};
+})();
 
 String.prototype.capitalize = String.prototype.capitalize || function() {
 	return this.charAt(0).toUpperCase() + this.substring(1);
