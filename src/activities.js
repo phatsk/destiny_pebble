7e931e360bfbf97faa546b6042f0f8d759658e74
phatsk <phatsk@gmail.com>
2015-04-01 09:29:15 -0400

    Remove deprecated AjaxWait

--- src/activities.js
+++ src/activities.js
@@ -1,240 +1,238 @@
 var activites = (function(){ 
-	var today = new Date();
-	var dp_util = require('dp_util');
-	var BUNGIE_API = require('bungie_api');
-	var ajax = require('ajax');
+	  var today = new Date();
+	  var dp_util = require('dp_util');
+	  var BUNGIE_API = require('bungie_api');
+	  var ajax = require('ajax');
 
-	var activityData = false;
+	  var activityData = false;
 
-	/**
-	 * First we try to get activity data from localStorage. This may be invalidated if 
-	 * #1 It's Tuesday and we haven't pulled the latest data after reset or
-	 * #2 We need to grab new daily info
-	 * @TODO Needs weekly event (Iron Banner, Queen's Wrath, etc)
-	 */
-	try
-	{
-		activityData = localStorage.getItem('activityData');
-		activityData = JSON.parse(activityData);
+	  /**
+	   * First we try to get activity data from localStorage. This may be invalidated if 
+	   * #1 It's Tuesday and we haven't pulled the latest data after reset or
+	   * #2 We need to grab new daily info
+	   * @TODO Needs weekly event (Iron Banner, Queen's Wrath, etc)
+	   */
+	  try
+	  {
+		    activityData = localStorage.getItem('activityData');
+		    activityData = JSON.parse(activityData);
 
-		if(activityData)
-		{
-			var nfreset;
-			var dailyreset;
+		    if(activityData)
+		    {
+			      var nfreset;
+			      var dailyreset;
 
-			// Reset for Tuesday - weeklies reset together, so we can check against the Nightfall
-			if(today.getDay() == 2 && (nfreset = new Date(activityData.Response.data.nightfallResetDate)) && today > nfreset) // Tuesday
-			{
-				throw "Weekly data is stale, pulling some fresh";
-			}
+			      // Reset for Tuesday - weeklies reset together, so we can check against the Nightfall
+			      if(today.getDay() == 2 && (nfreset = new Date(activityData.Response.data.nightfallResetDate)) && today > nfreset) // Tuesday
+			      {
+				        throw "Weekly data is stale, pulling some fresh";
+			      }
 
-			if((dailyreset = new Date(activityData.Response.data.dailyChapterResetDate)) && today > dailyreset)
-			{
-				throw "Daily data is stale, pulling some fresh";
-			}
-		}
-	}
-	catch(e) {
-		dp_util.logError('Tried (unsuccessfully) to grab local data: ' + e);
-		activityData = false;
-	}
+			      if((dailyreset = new Date(activityData.Response.data.dailyChapterResetDate)) && today > dailyreset)
+			      {
+				        throw "Daily data is stale, pulling some fresh";
+			      }
+		    }
+	  }
+	  catch(e) {
+		    dp_util.logError('Tried (unsuccessfully) to grab local data: ' + e);
+		    activityData = false;
+	  }
 
-	function updateActivities(menu, menuConfig)
-	{
-		var nightfallHash = 'activity-nightfall-' + activityData.Response.data.nightfallActivityHash;
-		var weeklyHash = 'activity-weekly-' + activityData.Response.data.heroicStrikeHashes.join('-');
-		var dailyHash = 'activity-daily-' + activityData.Response.data.dailyChapterHashes.join('-');
-		var crucibleHash = 'activity-crucible-' + activityData.Response.data.dailyCrucibleHash;
+	  function updateActivities(menu, menuConfig)
+	  {
+		    var nightfallHash = 'activity-nightfall-' + activityData.Response.data.nightfallActivityHash;
+		    var weeklyHash = 'activity-weekly-' + activityData.Response.data.heroicStrikeHashes.join('-');
+		    var dailyHash = 'activity-daily-' + activityData.Response.data.dailyChapterHashes.join('-');
+		    var crucibleHash = 'activity-crucible-' + activityData.Response.data.dailyCrucibleHash;
 
-		getLocalData(nightfallHash, function(data){
-			var item = {
-				subtitle: data.Response.data.activity.activityDescription,
-				userdata: {
-					key: nightfallHash,
-					getDetails: function() {
-						return {
-							title: 'Weekly Nightfall',
-							subtitle: 'Strike',
-							body: data.Response.data.activity.activityDescription
-						};
-					}
-				}
-			};
+		    getLocalData(nightfallHash, function(data){
+			      var item = {
+				        subtitle: data.Response.data.activity.activityDescription,
+				        userdata: {
+					          key: nightfallHash,
+					          getDetails: function() {
+						            return {
+							              title: 'Weekly Nightfall',
+							              subtitle: 'Strike',
+							              body: data.Response.data.activity.activityDescription
+						            };
+					          }
+				        }
+			      };
 
-			updateActivityMenu('nightfall', item, menu, menuConfig);
-		});
+			      updateActivityMenu('nightfall', item, menu, menuConfig);
+		    });
 
-		getLocalData(weeklyHash, function(data){
-			var item = {
-				subtitle: data.Response.data.activity.activityDescription,
-				userdata: {
-					key: weeklyHash,
-					getDetails: function() {
-						return {
-							title: 'Weekly Heroic',
-							subtitle: 'Strike',
-							body: data.Response.data.activity.activityDescription
-						};
-					}
-				}
-			};
+		    getLocalData(weeklyHash, function(data){
+			      var item = {
+				        subtitle: data.Response.data.activity.activityDescription,
+				        userdata: {
+					          key: weeklyHash,
+					          getDetails: function() {
+						            return {
+							              title: 'Weekly Heroic',
+							              subtitle: 'Strike',
+							              body: data.Response.data.activity.activityDescription
+						            };
+					          }
+				        }
+			      };
 
-			updateActivityMenu('weekly', item, menu, menuConfig);
-		});
+			      updateActivityMenu('weekly', item, menu, menuConfig);
+		    });
 
-		getLocalData(dailyHash, function(data){
-			var item = {
-				title: data.Response.data.activity.activityName,
-				subtitle: data.Response.data.activity.activityDescription,
-				userdata: {
-					key: dailyHash,
-					getDetails: function() {
-						return {
-							title: 'Daily Heroic',
-							subtitle: data.Response.data.activity.activityName,
-							body: data.Response.data.activity.activityDescription
-						};
-					}
-				}
-			};
+		    getLocalData(dailyHash, function(data){
+			      var item = {
+				        title: data.Response.data.activity.activityName,
+				        subtitle: data.Response.data.activity.activityDescription,
+				        userdata: {
+					          key: dailyHash,
+					          getDetails: function() {
+						            return {
+							              title: 'Daily Heroic',
+							              subtitle: data.Response.data.activity.activityName,
+							              body: data.Response.data.activity.activityDescription
+						            };
+					          }
+				        }
+			      };
 
-			updateActivityMenu('daily', item, menu, menuConfig);
-		});
+			      updateActivityMenu('daily', item, menu, menuConfig);
+		    });
 
-		getLocalData(crucibleHash, function(data){
-			var activityHash = data.Response.data.activity.activityTypeHash;
-			var activityType = data.Response.definitions.activityTypes[activityHash].activityTypeName;
+		    getLocalData(crucibleHash, function(data){
+			      var activityHash = data.Response.data.activity.activityTypeHash;
+			      var activityType = data.Response.definitions.activityTypes[activityHash].activityTypeName;
 
-			var item = {
-				title: activityType + ': ' + data.Response.data.activity.activityName,
-				subtitle: data.Response.data.activity.activityDescription,
-				userdata: {
-					key: crucibleHash,
-					getDetails: function() {
-						return {
-							title: activityType,
-							subtitle: data.Response.data.activity.activityName,
-							body: data.Response.data.activity.activityDescription
-						};
-					}
-				}
-			};
+			      var item = {
+				        title: activityType + ': ' + data.Response.data.activity.activityName,
+				        subtitle: data.Response.data.activity.activityDescription,
+				        userdata: {
+					          key: crucibleHash,
+					          getDetails: function() {
+						            return {
+							              title: activityType,
+							              subtitle: data.Response.data.activity.activityName,
+							              body: data.Response.data.activity.activityDescription
+						            };
+					          }
+				        }
+			      };
 
-			updateActivityMenu('crucible', item, menu, menuConfig);
-		});
-	}
+			      updateActivityMenu('crucible', item, menu, menuConfig);
+		    });
+	  }
 
-	function getLocalData(hash, callback)
-	{
-		var data;
-		callback = callback || function() {};
+	  function getLocalData(hash, callback)
+	  {
+		    var data;
+		    callback = callback || function() {};
 
-		dp_util.logInfo('Getting local data for hash: ' + hash);
+		    dp_util.logInfo('Getting local data for hash: ' + hash);
 
-		try
-		{
-			data = localStorage.getItem(hash);
-			data = JSON.parse(data);
+		    try
+		    {
+			      data = localStorage.getItem(hash);
+			      data = JSON.parse(data);
 
-			dp_util.logLocal('Data gathered from localStorage for hash `' + hash + '`: ' + JSON.stringify(data));
+			      dp_util.logLocal('Data gathered from localStorage for hash `' + hash + '`: ' + JSON.stringify(data));
 
-			if(!data || !data.Response.definitions)
-				throw 'No activity data for ' + hash + ', fetching fresh data';
+			      if(!data || !data.Response.definitions)
+				        throw 'No activity data for ' + hash + ', fetching fresh data';
 
-			if(dp_util.get('CACHE_INVALIDATE'))
-				throw 'Manually flushing cache!';
-		}
-		catch(e) {
-			var activityUrl = BUNGIE_API.MANIFEST.ACTIVITY + hash.split('-')[hash.split('-').length-1];
-			dp_util.logError('Tried (unsuccessfully) to grab local data for ' + hash + ': ' + e);
-			dp_util.logInfo('Pulling data for ' + hash + ' from: ' + activityUrl);
+			      if(dp_util.get('CACHE_INVALIDATE'))
+				        throw 'Manually flushing cache!';
+		    }
+		    catch(e) {
+			      var activityUrl = BUNGIE_API.MANIFEST.ACTIVITY + hash.split('-')[hash.split('-').length-1];
+			      dp_util.logError('Tried (unsuccessfully) to grab local data for ' + hash + ': ' + e);
+			      dp_util.logInfo('Pulling data for ' + hash + ' from: ' + activityUrl);
 
-			AjaxWait();
+			      ajax({
+				        url: activityUrl + '?definitions=true',
+				        type: 'json'
+			      }, function(data){
+				        localStorage.setItem(hash, JSON.stringify(data));
+				        ClearWait();
 
-			ajax({
-				url: activityUrl + '?definitions=true',
-				type: 'json'
-			}, function(data){
-				localStorage.setItem(hash, JSON.stringify(data));
-				ClearWait();
+				        dp_util.logRemote('Got remote activity data: ' + JSON.stringify(data));
 
-				dp_util.logRemote('Got remote activity data: ' + JSON.stringify(data));
+				        callback(data);
+			      }, function(error){
+				        ClearWait();
 
-				callback(data);
-			}, function(error){
-				ClearWait();
+				        dp_util.logError('Could not get response from ' +  activityUrl + ': ' + error);
+			      });
+		    }
 
-				dp_util.logError('Could not get response from ' +  activityUrl + ': ' + error);
-			});
-		}
+		    if(data)
+		    {
+			      dp_util.logLocal('Found local data for hash ' + hash + ', executing callback');
+			      callback(data);
+		    }
+	  }
 
-		if(data)
-		{
-			dp_util.logLocal('Found local data for hash ' + hash + ', executing callback');
-			callback(data);
-		}
-	}
+	  function updateActivityMenu(activity, item, menu, menuConfig)
+	  {
+		    var sections = [];
+		    var key;
 
-	function updateActivityMenu(activity, item, menu, menuConfig)
-	{
-		var sections = [];
-		var key;
+		    if(item)
+		    {
+			      dp_util.logUI('Updating menu for ' + activity);
 
-		if(item)
-		{
-			dp_util.logUI('Updating menu for ' + activity);
+			      for(key in item)
+			      {
+				        menuConfig[activity][key] = item[key];
+			      }
+		    }
 
-			for(key in item)
-			{
-				menuConfig[activity][key] = item[key];
-			}
-		}
+		    dp_util.logUI('Updating activity menu: ' + JSON.stringify(menuConfig));
 
-		dp_util.logUI('Updating activity menu: ' + JSON.stringify(menuConfig));
+		    for(key in menuConfig)
+		    {
+			      if(menuConfig.hasOwnProperty(key))
+			      {
+				        sections.push(menuConfig[key]);
+			      }
+		    }
 
-		for(key in menuConfig)
-		{
-			if(menuConfig.hasOwnProperty(key))
-			{
-				sections.push(menuConfig[key]);
-			}
-		}
+		    dp_util.logUI('New menu sections: ' + JSON.stringify(sections));
+		    menu.section(0, {title: 'Activities', items: sections});
+	  }
 
-		dp_util.logUI('New menu sections: ' + JSON.stringify(sections));
-		menu.section(0, {title: 'Activities', items: sections});
-	}
+	  return {
+		    init: function(menu, menuConfig) {
+			      var ajax = require('ajax');
+			      /**
+			       * We didn't get any valid activityData so let's pull
+			       * fresh data from Bungie
+			       */
+			      if(!activityData || !activityData.Response.definitions || dp_util.get('CACHE_INVALIDATE'))
+			      {
+				        ajax({
+					          url: BUNGIE_API.ADVISORS,
+					          type: 'json'
+				        }, function(data){
+					          activityData = data;
+					          localStorage.setItem('activityData', JSON.stringify(data));
 
-	return {
-		init: function(menu, menuConfig) {
-			var ajax = require('ajax');
-			/**
-			* We didn't get any valid activityData so let's pull
-			* fresh data from Bungie
-			*/
-			if(!activityData || !activityData.Response.definitions || dp_util.get('CACHE_INVALIDATE'))
-			{
-				ajax({
-					url: BUNGIE_API.ADVISORS,
-					type: 'json'
-				}, function(data){
-					activityData = data;
-					localStorage.setItem('activityData', JSON.stringify(data));
+					          dp_util.logRemote('Got remote activity data: ' + JSON.stringify(data));
 
-					dp_util.logRemote('Got remote activity data: ' + JSON.stringify(data));
+					          updateActivities(menu, menuConfig);
+				        }, function(error){
 
-					updateActivities(menu, menuConfig);
-				}, function(error){
-
-					dp_util.logError('Could not get response from ' +  BUNGIE_API.ADVISORS + ': ' + error);
-				});
-			}
-			else
-			{
-				dp_util.logLocal('Got locally stored activity data: ' + JSON.stringify(activityData.Response.data.nightfallActivityHash));
-				updateActivities(menu, menuConfig);
-			}
-		}	
-	};
+					          dp_util.logError('Could not get response from ' +  BUNGIE_API.ADVISORS + ': ' + error);
+				        });
+			      }
+			      else
+			      {
+				        dp_util.logLocal('Got locally stored activity data: ' + JSON.stringify(activityData.Response.data.nightfallActivityHash));
+				        updateActivities(menu, menuConfig);
+			      }
+		    }	
+	  };
 })();
 
 this.exports = activites;
--- src/app.js
+++ src/app.js
@@ -8,101 +8,95 @@ var ui = require('ui');
 var ajax = require('ajax');
 
 var DetailWindow = new ui.Card({
-	title: 'Loading...',
-	scrollable: true
+	  title: 'Loading...',
+	  scrollable: true
 });
 
 var MainMenu = new ui.Menu({
-	sections: [{
-		title: 'Activities',
-		items: {
-			title: 'Loading...'
-		}
-	},{
-		title: 'Guardian'
-	}]
+	  sections: [{
+		    title: 'Activities',
+		    items: {
+			      title: 'Loading...'
+		    }
+	  },{
+		    title: 'Guardian'
+	  }]
 });
 
 activities.init(MainMenu, {
-	nightfall: {
-		title: 'Weekly Nightfall',
-		subtitle: 'Loading...',
-		userdata: {
-			key: 'NIGHTFALL'
-		}
-	},
-	weekly: {
-		title: 'Weekly Heroic',
-		subtitle: 'Loading...',
-		userdata: {
-			key: 'WEEKLY'
-		}
-	},
-	daily: {
-		title: 'Daily Heroic',
-		subtitle: 'Loading...',
-		userdata: {
-			key: 'DAILY'
-		}
-	},
-	crucible: {
-		title: 'Daily Crucible',
-		subtitle: 'Loading...',
-		userdata: {
-			key: 'CRUCIBLE'
-		}
-	}
+	  nightfall: {
+		    title: 'Weekly Nightfall',
+		    subtitle: 'Loading...',
+		    userdata: {
+			      key: 'NIGHTFALL'
+		    }
+	  },
+	  weekly: {
+		    title: 'Weekly Heroic',
+		    subtitle: 'Loading...',
+		    userdata: {
+			      key: 'WEEKLY'
+		    }
+	  },
+	  daily: {
+		    title: 'Daily Heroic',
+		    subtitle: 'Loading...',
+		    userdata: {
+			      key: 'DAILY'
+		    }
+	  },
+	  crucible: {
+		    title: 'Daily Crucible',
+		    subtitle: 'Loading...',
+		    userdata: {
+			      key: 'CRUCIBLE'
+		    }
+	  }
 });
 
 guardian.init(MainMenu);
 
 MainMenu.on('select', function(event){
-	var sections = event.item.userdata.getDetails();
+	  var sections = event.item.userdata.getDetails();
 
-	dp_util.logUI('Showing details for ' + event.item.userdata.key);
+	  dp_util.logUI('Showing details for ' + event.item.userdata.key);
 
-	for(var key in sections)
-	{
-		if(sections.hasOwnProperty(key) && DetailWindow[key])
-		{
-			DetailWindow[key](sections[key]);
-		}
-	}
+	  for(var key in sections)
+	  {
+		    if(sections.hasOwnProperty(key) && DetailWindow[key])
+		    {
+			      DetailWindow[key](sections[key]);
+		    }
+	  }
 
-	DetailWindow.show();
+	  DetailWindow.show();
 });
 
 var waitCard = new ui.Card({
-	title: 'Loading...'
+	  title: 'Loading...'
 });
 
 var activityData;
 
 MainMenu.show();
 
-function AjaxWait()
-{
-	waitCard.show();
-	MainMenu.hide();
-}
-
 function ClearWait()
 {
-	MainMenu.show();
-	waitCard.hide();
+	  MainMenu.show();
+	  waitCard.hide();
 }
 
 // configuration stuffs
 Pebble.addEventListener('showConfiguration', function(e){
-	Pebble.openURL('http://destiny-phatsk.rhcloud.com/config.php');
+	  Pebble.openURL('http://destiny-phatsk.rhcloud.com/config.php');
 });
 
 Pebble.addEventListener('webviewclosed', function(e){
-	var config = JSON.parse(decodeURIComponent(e.response));
+	  var config = JSON.parse(decodeURIComponent(e.response));
     dp_util.logLocal('Configuration window returned: ', JSON.stringify(config));
 
-	if(config)
-	{
-		localStorage.setItem('guardian_config', JSON.stringify(config));
-	}
+	  if(config)
+	  {
+		    localStorage.setItem('guardian_config', JSON.stringify(config));
+	  }
 });
